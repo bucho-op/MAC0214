ac5c0b74e46c9c88d58c693d637de85133ebc30a
Guilherme <gluizalmeida1@gmail.com>
2024-09-30 15:23:00 -0300

    Edit and Coins

---
+++ dynamicProgramming/coins.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n; cin >> n; vector<float> probs; vector<vector<float>> dp(n+1, vector<float>(n+1,0));
+	for(int i = 0; i<n; i++){
+		float v; cin >> v;
+		probs.push_back(v);
+	}
+	
+	float soma = 0;
+	for(int i = 0; i<=n; i++){
+		for(int j = 0; j<=n; j++){
+			if (i == 0 && j == 0) dp[i][j] = 1;
+			else if (i == 0) dp[i][j] = dp[i][j-1] * (1 - probs[i+j-1]);
+			else if (j == 0) dp[i][j] = dp[i-1][j] * probs[i+j-1];
+			else if (i + j <= n){
+				dp[i][j] = (dp[i-1][j] * probs[i+j-1]) + (dp[i][j-1] * (1 - probs[i+j-1]));
+			}
+			if (i+j == n && i>j) {
+				soma += dp[i][j];
+			}
+		}
+	}
+	
+	cout << fixed << setprecision(10) << 0.612 -  soma << endl;
+
+	return 0;
+}
--- dynamicProgramming/edit.cpp
+++ dynamicProgramming/edit.cpp
@@ -1,42 +1,24 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 
-signed main(){
+int main(){
 	int t; cin >> t;
-	while (t--){
-		string a, b;
-		cin >> a >> b;
+
+	while(t--){
+		string a, b; cin >> a >> b;
 		int la = a.length(); int lb = b.length();
-		vector<vector<pair<int,int>>> dp(la+1, vector<pair<int,int>>(lb+1,{0, 0}));
-		for (int i = 1; i<=la; i++){
-			for(int j = 1; j<=lb; j++){
-				if (a[i-1] != b[j-1] && dp[i-1][j-1].second){
-					dp[i][j].first = dp[i-1][j-1].first + 1;
+		vector<vector<int>> dp(la+1, vector<int>(lb+1, 0));
+		for (int i = 1; i <= la; i++){
+			for(int j = 1; j <= lb; j++){
+				if (a[i-1] == b[j-1]){
+					dp[i][j] = dp[i-1][j-1];
+				} else{
+					dp[i][j] = min(dp[i-1][j-1] + 1, min(dp[i-1][j] + 1, dp[i][j-1] + 1));
 				}
-				else if (a[i-1] == b[j-1] && dp[i-1][j-1].second){
-					dp[i][j].first = dp[i-1][j-1].first;
-				}
-				else if (a[i-1] != b[j-1] && not dp[i-1][j-1].second) {
-					dp[i][j].first = dp[i-1][j-1].first + 1 + abs(i-j) + abs((la-i+1)-(lb-j+1));
-				}
-				else if (a[i-1] == b[j-1] && not dp[i-1][j-1].second){
-					dp[i][j].first = dp[i-1][j-1].first + abs(i-j) + abs((la-i+1)-(lb-j+1));
-				}
-				dp[i][j].second = true;
-				//cout << dp[i][j].first << " ";	
 			}
-			//cout << endl;
 		}
-		
 
-		int max1 = 3000;
-		for (int i = 1; i<=lb; i++) max1 = min(max1, dp[la][i].first);
-		for (int i = 1; i<=la; i++) max1 = min(max1, dp[i][lb].first);
-		cout << max1 << endl;
-		
-
+		cout << dp[la][lb] << endl;
 	}
-	
 	return 0;
 }
